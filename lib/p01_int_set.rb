332a2796a8a1f35bf26e8e017dd3da257ab75006
ShengzhiLuo <sl5009@cumc.columbia.edu>
2022-06-20 22:15:02 -0400

    w5d1

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,104 @@
+class MaxIntSet
+  attr_accessor :max , :store
+  def initialize(max)
+    @store = Array.new(max,false)
+    @max = max
+  end
+
+  def insert(num)
+    validate!(num)
+    return false if @store[num]
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    return nil if !self.include?(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    num > 0 && num < @max
+  end
+
+  def validate!(num)
+    raise "Out of bounds" if !is_valid?(num)
+  end
+end
+
+
+class IntSet
+  attr_reader :store
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    return false if include?(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    return nil if !include?(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    if !self.include?(num)
+      self[num] << num
+      count += 1
+    end
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    self[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
